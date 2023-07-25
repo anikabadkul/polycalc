c6dd1a246b823377797322e7e3ca0e73c8b4c127
anikabadkul <116839529+anikabadkul@users.noreply.github.com>
2023-07-25 18:49:24 +0530

    Add files via upload

---
+++ polynomial.py
@@ -0,0 +1,17 @@
+###############################
+# Team members- Please enter your names here
+# Name1:
+# Name2:
+###############################
+
+
+def display_menu():
+    """Display the list of polynomial available tools"""
+    print("1-Insert; 2-Remove; 3-Info; 4-Evaluate; 5-Scaling; 6-Derive; 7-Integrate")
+    print("8-Summation; 9-Subtract; 10-Multiply; 11-Divide")
+
+
+def display_list(list_poly):
+    # the instruction below is just a placeholder
+    # you must remove the instruction and modify/complete this function
+    list_poly=list_poly
